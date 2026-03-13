2a701ade514bb3fa4d5f32dfbbbc0a21bc2d5d01
hristo.hristov <hristo.hristov@microinvest.net>
2026-03-13 13:55:03 +0200

    Day 1: Clean Architecture solution structure + Domain layer

--- CryptoTracker.Domain/Entities/Alert.cs
+++ CryptoTracker.Domain/Entities/Alert.cs
@@ -10,12 +10,15 @@ namespace CryptoTracker.Domain.Entities
 
         public AlertDirection Direction { get; set; }
 
-        public AlertStatus Status { get; set; }
+        public AlertStatus Status { get; set; } = AlertStatus.Active;
 
         public DateTime CreatedAt{ get; set; }
 
         public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
 
         public Guid AssetId { get; set; }
+
+        public Asset Asset { get; set; } = null!;
     }
 }
