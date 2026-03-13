ea32b88e844b67b4a1a4d8e12aa40e13ead51acd
hrist <hrist@DESKTOP-226RI5Q>
2026-03-13 08:13:09 +0200

    added DBContext

---
+++ CryptoTracker.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CryptoTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoTracker.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Asset> Assets { get; }
+
+        DbSet<User> Users { get; }
+
+        DbSet<Alert> Alerts { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ CryptoTracker.Domain/Entities/Alert.cs
@@ -0,0 +1,21 @@
+﻿using CryptoTracker.Domain.Enums;
+
+namespace CryptoTracker.Domain.Entities
+{
+    public class Alert
+    {
+        public Guid Id { get; set; }
+        
+        public decimal TargetPrice { get; set; }
+
+        public AlertDirection Direction { get; set; }
+
+        public AlertStatus Status { get; set; }
+
+        public DateTime CreatedAt{ get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid AssetId { get; set; }
+    }
+}
